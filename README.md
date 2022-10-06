1fd1501f936fbee45cba929df892d31a88cfa306
Omena0 <82773737+Omena0@users.noreply.github.com>
2022-10-06 19:19:00 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # MSG APP V3
- A message ap with token auth
+### A message app with token auth.
+#### Basically i control everyones accounts lol
+##### And only i can make accounts
+###### But its more secure than nothing
+###### I will add encryption if you pay me 100€ rn
+###### But that will make an entirely new version tbh
+###### Get hyped for V4
+
+It basically connects to auth server and sends hashed password + username and if they correct the server generate the client a token that they can use to send messages... Yes you need token for EVERY message lol
+
+## Features:
+
+- [ ] Encryption, and i mean any encryption...
+- [X] Hashing!
+- [X] Security (more than private v2 with no auth)
+- [X] Being pro gamer
+- [X] Closed source because i dont want to share any server code!
+- [X] Open source CLIENT
---
+++ client.py
@@ -0,0 +1,91 @@
+print('Starting....')
+
+import lib
+lib.log('*','LIB Initialized..')
+lib.log('*','Initializing [Socket, Threading, Sys]...')
+import socket
+from threading import Thread
+import sys
+import os
+
+#Example Token: ('2ade65e8b2860acdfa76ff7520c8aa785ddeb992', 'Omena0')
+
+def hang():
+    input('Press ENTER to close...')
+    os._exit(0)
+    sys.exit(0)
+
+authip   = '192.168.0.104'
+authport = 1234
+
+s = socket.socket()
+
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+lib.log('*','Connecting to AUTH-SERVER...')
+try: s.connect((authip,authport))
+except:
+    lib.log('!','Failed to connect to AUTH-SERVER!')
+    hang()
+
+username = input('Username: ')
+password = input('Password: ')
+
+lib.log('*','Authenticating...')
+
+s.send(f'? {username} {lib.hash(password)}'.encode())
+
+answer = s.recv(32767).decode().replace('X ','')
+
+if answer == 'Invalid_Password':
+    lib.log('!','Invalid Password! (X Invalid_Password)')
+    hang()
+elif answer == 'No_User':
+    lib.log('!','Invalid Username! (X No_User)')
+    hang()
+else:
+    token = answer
+    print(token)
+    lib.log('+',f'Logged in as {username}')
+
+s = socket.socket()
+
+ip = input('IP: ')
+port = input('PORT: ')
+
+if ip == '': ip = '192.168.0.104'
+if port == '': port = 5000
+
+if ip == authip and port == authport:
+    lib.log('!','IP and PORT cannot be the same as AUTH-ADDRESS.')
+    hang()
+
+def message_handler():
+    while True:
+        s.send((input()+'<TOKEN>'+token).encode())
+
+
+
+s.connect((ip,int(port)))
+s.send(('<TOKEN>'+token).encode())
+
+a = Thread(target=message_handler)
+a.daemon = True
+a.start()
+
+while True:
+    msg = s.recv(2096).decode()
+    msg = msg.split('<SEP>')
+    lib.log(msg[0],msg[1])
+
+
+
+
+
+
+
+
+
+
+
+
